1ac02f0bfe6dd3bca4fe8e2b58644c0eabcfee2e
kennencool <3304192108@qq.com>
2021-04-16 13:32:06 +0800

    Functional test now checks we can input a to-do item

--- suplerlists/functional_tests.py
+++ suplerlists/functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):   # (1)
@@ -14,23 +16,36 @@ class NewVisitorTest(unittest.TestCase):   # (1)
         # to check out its homepage
         self.browser.get('http://localhost:8000')
 
-# she notices the page title and header mention to-do lists
+        # she notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)  # (4)
-        self.fail('Finish the test!')  # (5)
+        header_text = self.browser.find_element_by_tag_name('hi').text
+        self.assertIn('!To-Do', header_text)  # (5)
 
-if __name__ == '__main__':  # (6)
-    unittest.main(warnings='ignore')  # (7)
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
-# She is invited to enter a to-do item straight away
+        # She types "Buy peacock feathers" into a text box(Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
-# She types "Buy peacock feathers" into a text box(Edith's hobby
-# is tying fly-fishing lures)
+        # when she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-# when she hits enter, the page updates, and now the page lists
-# "1: Buy peacock feathers" as an item in a to-do list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
-# There is still a text box inviting her to add another item. She
-# enters "User peacock feathers to make a fly" (Edith is very methodical)
+        # There is still a text box inviting her to add another item. She
+        # enters "User peacock feathers to make a fly" (Edith is very methodical)
+        self.fail('Finish the test!')
 
 # The page updates again, and now shows both items on her list
 
@@ -41,3 +56,6 @@ if __name__ == '__main__':  # (6)
 # She visits that URL - her to-do list is still there.
 
 # Satisfied, she goes back to sleep
+
+if __name__ == '__main__':  # (6)
+    unittest.main(warnings='ignore')  # (7)
